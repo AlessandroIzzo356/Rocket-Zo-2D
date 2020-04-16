fe1a17784210bfdb985b8d422d16910654c38de9
AlessandroIzzo356 <Alessandroizzo39@gmail.com>
2020-04-16 20:32:11 +0200

    RUN

---
+++ Assets/Scripts/ObstacleScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleScript : MonoBehaviour
+{
+    public GameObject backgroundPos;
+
+    void Start()
+    {
+        
+    }
+
+    void ObstacleMove(){
+        Vector2 pos;
+        pos = new Vector2(0, transform.position.y + backgroundPos.transform.position.y);
+    }
+
+    
+    void Update()
+    {
+        ObstacleMove();
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerScript : MonoBehaviour {
+
+    float speed = 5f;
+    float directionSpeed = 20f;
+    Rigidbody2D rigidbody2D;
+    
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void PlayerController()
+    {
+        float hor = Input.GetAxis("Horizontal");
+       transform.position = Vector3.Lerp(gameObject.transform.position, new Vector3(Mathf.Clamp(gameObject.transform.position.x + hor, -8f, 8f), gameObject.transform.position.y, gameObject.transform.position.z), directionSpeed * Time.deltaTime);
+       //rigidbody2D.velocity = new Vector2(0, speed);
+    }
+
+ 
+    void Update()
+    {
+        PlayerController();
+    }
+}
